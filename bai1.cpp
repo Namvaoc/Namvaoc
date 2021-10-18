2e0d8175fabfe464514f3c19fbb16751459df5c4
namvaoc <hainam7076@gmail.com>
2021-10-18 23:24:03 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+	std::cout << "C Language Basic Syntax Rules:" << std::endl;
+	std::cout << "\t" << "1. Lowercase letters are significant" << std::endl;
+	std::cout << "\t" << "2. main is where program execution begins"<< std::endl;
+	std::cout << "\t" << "3. Opening and closing braces enclose program statements in a routine"<< std::endl;
+	std::cout << "\t" << "4. All program statement must be terminated by a semicolon" << std::endl;
+	return 0;
+}
---
+++ baitap3_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main ()
+{
+	int a,b;
+	std::cout << "Nhap so thu nhat: "; std::cin >> a;
+	std::cout << "Nhap so thu hai: "; std::cin >> b;
+	std::cout << "Hieu a va b la:"<< a-b;
+	return 0;
+}
+
